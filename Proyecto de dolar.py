7777e51fd2f212709095526b8109dbfd4dbe95c1
Julio Guerrero <79754619+JDGR33@users.noreply.github.com>
2022-01-06 13:49:00 -0400

    Gets the data every 60 seconds
    Gets the data very 60 seconds

--- Proyecto de dolar.py
+++ Proyecto de dolar.py
@@ -8,24 +8,51 @@ Created on Wed Dec 15 13:57:22 2021
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 #Importing date library
-from datetime import date
+import datetime
 #Import DataBase libraries
-import  ibm_db
+#import  ibm_db
 
-#Url of the Central Bank of Venezuela
-html = urlopen('http://www.bcv.org.ve/')
+#Import librery for Schedule events
+import sched, time
 
-#Web Screpaing the Converation Rate of $ to BS
-bs = BeautifulSoup(html.read(),'html.parser')
-dolarBox = bs.find(id="dolar").find ('div',{'class':"col-sm-6 col-xs-6 centrado"})
-
-#Transforming the text base converation rate to a floar number
-dolarText = dolarBox.get_text().replace(",",".")
-convertaionRate = float(dolarText)
-
-#Formating the date of the data point
-d1 = date.today().strftime("%d/%m/%Y")
+schedule = sched.scheduler(time.time, time.sleep)
 
 
+#Retturns the exchange rate and the date-time of the extraction
+def get_conversion_bcv():
+
+    #Url of the Central Bank of Venezuela
+    html = urlopen('http://www.bcv.org.ve/')
+    
+    #Web Screpaing the Converation Rate of $ to BS
+    bs = BeautifulSoup(html.read(),'html.parser')
+    dolarBox = bs.find(id="dolar").find ('div',{'class':"col-sm-6 col-xs-6 centrado"})
+    
+    #Transforming the text base converation rate to a floar number
+    dolarText = dolarBox.get_text().replace(",",".")
+    exchangeRate = float(dolarText)
+    
+    #Formating the date and time of the data point
+    extractionDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    print("The convertarion Rate on ",extractionDate," was ", exchangeRate)
+    
+    return(exchangeRate,extractionDate)
+
+
+#Schedule the reading of data
+def do_something(delay=1): 
+    print("Doing stuff...")
+    #Prints the date and exchange Rate
+    get_conversion_bcv()
+    #this begins the loop again
+    schedule.enter(delay, 1, do_something,argument=(delay,))
+    
+
+
+delay = 60# Delay of the reading
+#Beginings the loop
+schedule.enter(delay, 1, do_something,argument=(delay,))
+
+schedule.run() #Runs the Schdule events
 #Prints the date and converation rate
-print("The convertarion Rate on ",d1," was ", convertaionRate)
+rate,date = get_conversion_bcv()
